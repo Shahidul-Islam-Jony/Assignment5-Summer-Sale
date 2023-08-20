c3a59282ee53efaa46052861ca2c953670b42b7c
Shahidul-Islam <shahidulislam13sf@gmail.com>
2023-08-21 00:22:15 +0600

    discount calculation done

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ function addToCart(data){
    const addItems = document.getElementById('add-items');
    const p = document.createElement('p');
    const count = addItems.childElementCount;
-   p.innerHTML = `${count+1} . ${Name}`;
+   p.innerHTML = `${count+1}. ${Name}`;
    addItems.appendChild(p);
 
    const makePurchaseBtn = document.getElementById('make-purchase');
@@ -18,9 +18,25 @@ function totalPrice(price){
     const previousTotalPriceField = document.getElementById('total-price');
     const previousTotalPrice = previousTotalPriceField.innerText;
     const currentTotalPrice = parseFloat(previousTotalPrice) + parseFloat(price);
-    previousTotalPriceField.innerText = currentTotalPrice;
+    previousTotalPriceField.innerText = currentTotalPrice.toFixed(2);
     if(currentTotalPrice >= 200){
         const applyButton = document.getElementById('apply-btn');
         applyButton.removeAttribute('disabled');
     }
 }
+
+document.getElementById('apply-btn').addEventListener('click',function(){
+    const inputField = document.getElementById('input-field');
+    const inputValue = inputField.value;
+    if(inputValue === 'SELL200'){
+        const totalPriceInString = document.getElementById('total-price').innerText;
+        const totalPrice = parseFloat(totalPriceInString);
+        
+        const discount = (totalPrice/100)*20;
+        document.getElementById('discount-price').innerText = discount.toFixed(2);
+        document.getElementById('total').innerText = (totalPrice - discount).toFixed(2) ;
+    }
+    else{
+        alert('Wrong coupon!!!  please enter valid coupon code');
+    }
+})
